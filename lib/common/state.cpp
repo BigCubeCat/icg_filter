29993a20f964bc5912c577f0d877d66693f719f3
BigCubeCat <bigcubecat@yandex.ru>
2025-03-01 23:53:15 +0700

    fix:

--- lib/common/state.cpp
+++ lib/common/state.cpp
@@ -1,21 +1,19 @@
 #include "state.hpp"
 
+#include <QFileInfo>
+#include <utility>
+
 StateSingleton& StateSingleton::instance() {
     static StateSingleton inst;
     return inst;
 }
 
-int StateSingleton::width() const {
-    return m_width;
+void StateSingleton::setController(SignalController* controller) {
+    m_controller_ptr = controller;
 }
 
-int StateSingleton::height() const {
-    return m_height;
-}
-
-void StateSingleton::setGeometry(int w, int h) {
-    m_width = w;
-    m_height = h;
+SignalController* StateSingleton::controller() {
+    return m_controller_ptr;
 }
 
 void StateSingleton::selectFilter(e_filter filter) {
@@ -24,4 +22,36 @@ void StateSingleton::selectFilter(e_filter filter) {
 
 e_filter StateSingleton::currentFilter() const {
     return m_selected_filter;
+}
+
+void StateSingleton::setFilename(std::string filename) {
+    m_filename = std::move(filename);
+
+    QFileInfo file_info(QString::fromStdString(m_filename));
+    m_folder = file_info.absolutePath().toStdString();
+    m_file_format = file_info.completeSuffix().toStdString();
+}
+
+std::string StateSingleton::filename() const {
+    return m_filename;
+}
+
+void StateSingleton::setFileFormat(std::string format) {
+    m_file_format = std::move(format);
+}
+
+std::string StateSingleton::fileFormat() const {
+    return m_file_format;
+}
+
+std::string StateSingleton::folder() const {
+    return m_folder;
+}
+
+void StateSingleton::setImage(QImage new_image) {
+    m_image = std::move(new_image);
+}
+
+QImage& StateSingleton::image() {
+    return m_image;
 }
--- lib/common/state.hpp
+++ lib/common/state.hpp
@@ -3,12 +3,14 @@
  */
 #pragma once
 
+#include <QImage>
 #include <QJsonObject>
 #include <QObject>
 #include <mutex>
 #include <optional>
 #include <string>
 #include <unordered_map>
+#include "../signalcontroller/signalcontroller.hpp"
 #include "e_filter.hpp"
 
 /*!
@@ -18,15 +20,19 @@
  */
 class StateSingleton {
    private:
+    std::mutex m_mutex;
     StateSingleton() = default;
 
-    std::mutex m_mutex;
-    e_filter m_selected_filter;
-
-    int m_width = 600;
-    int m_height = 400;
+    SignalController* m_controller_ptr;
 
     std::unordered_map<std::string, QJsonObject> m_config;
+    e_filter m_selected_filter;
+
+    std::string m_filename;
+    std::string m_file_format;
+    std::string m_folder;
+    QImage m_prev_image;
+    QImage m_image;
 
    public:
     StateSingleton(const StateSingleton&) = delete;
@@ -35,12 +41,8 @@ class StateSingleton {
     /// вернуть сущность объекта
     static StateSingleton& instance();
 
-    /// установить геометрию
-    void setGeometry(int w, int h);
-    /// ширина холста
-    int width() const;
-    /// высота холста
-    int height() const;
+    void setController(SignalController* controller);
+    SignalController* controller();
 
     /// Задает конфиг
     void setConfig(const std::string& name, const QJsonObject& value);
@@ -51,4 +53,13 @@ class StateSingleton {
     void selectFilter(e_filter filter);
     /// возращает тэг текущего инструмента
     e_filter currentFilter() const;
+
+    void setFilename(std::string filename);
+    std::string filename() const;
+    void setFileFormat(std::string format);
+    std::string fileFormat() const;
+    std::string folder() const;
+
+    void setImage(QImage new_image);
+    QImage& image();
 };
