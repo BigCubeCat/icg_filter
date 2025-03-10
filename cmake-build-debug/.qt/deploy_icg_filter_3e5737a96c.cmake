0c1e2a8deaed38ce57d78a3a66d12ff3ec9289eb
AMChumak <i@amchumak.ru>
2025-03-11 04:29:38 +0700

    added cmake build in gitignore

--- cmake-build-debug/.qt/deploy_icg_filter_3e5737a96c.cmake
+++
@@ -1,8 +0,0 @@
-include(/home/anton/Documents/graphics/icg_filter/cmake-build-debug/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/icg_filter-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE /home/anton/Documents/graphics/icg_filter/cmake-build-debug/icg_filter
-    GENERATE_QT_CONF
-)
