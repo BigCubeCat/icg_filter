19b103d82d565d39c0a810b25481a33668671a2f
Биточкин Егор <e.bitochkin@g.nsu.ru>
2025-03-17 15:51:02 +0700

    feat: сделал zoom fit

--- lib/mainwindow/mainwindow.cpp
+++ lib/mainwindow/mainwindow.cpp
@@ -72,6 +72,8 @@ void MainWindow::connectSlots() {
             &ImageProcessor::zoomOut);
     connect(m_ui->actionResetZoom, &QAction::triggered, m_im,
             &ImageProcessor::zoomReset);
+    connect(m_ui->actionZoomFit, &QAction::triggered, this,
+            &MainWindow::zoomFit);
 
     connect(m_controller, &SignalController::saveFileSignal, this,
             &MainWindow::updateFilename);
@@ -173,4 +175,8 @@ void MainWindow::filterApplyed() {
 
 void MainWindow::hideToolbar() {
     m_ui->toolBar->setHidden(!m_ui->actionShowToolbar->isChecked());
+}
+
+void MainWindow::zoomFit() {
+    m_im->zoomFit(m_ui->scrollArea->size());
 }
