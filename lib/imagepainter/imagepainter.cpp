12df40d37825a17f00d57d108615ad1b03042c4c
Биточкин Егор <e.bitochkin@g.nsu.ru>
2025-03-24 16:46:09 +0700

    feat(imagepainter): УРА УРА

--- lib/imagepainter/imagepainter.cpp
+++ lib/imagepainter/imagepainter.cpp
@@ -1,17 +1,39 @@
 #include "imagepainter.hpp"
 #include <qdebug.h>
+#include <qgraphicsview.h>
 #include <qlogging.h>
 
-#include "ui_imagepainter.h"
-
 ImagePainter::ImagePainter(ImageProcessor* processor, QWidget* parent)
-    : QWidget(parent), m_processor(processor), m_ui(new Ui::ImagePainter) {
-    m_ui->setupUi(this);
+    : QGraphicsView(parent), m_processor(processor) {
+    setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
 }
 
 void ImagePainter::setView(const QImage& image) {
-    qDebug() << image.isNull();
-    auto* pixmap_item = new QGraphicsPixmapItem(QPixmap::fromImage(image));
-    m_scene.addItem(pixmap_item);
+    m_pixmap_item =
+        std::make_unique<QGraphicsPixmapItem>(QPixmap::fromImage(image));
+    m_scene.addItem(m_pixmap_item.get());
     m_scene.setSceneRect(image.rect());
+    this->setScene(&m_scene);
+}
+
+void ImagePainter::wheelEvent(QWheelEvent* event) {
+    if (event->angleDelta().y() > 0)
+        scale(1.1, 1.1);
+    else
+        scale(0.9, 0.9);
+}
+
+void ImagePainter::mousePressEvent(QMouseEvent* event) {
+    if (event->button() == Qt::LeftButton)
+        m_last_drag_pos = event->position();
+}
+
+void ImagePainter::mouseMoveEvent(QMouseEvent* event) {
+    if (event->buttons() & Qt::LeftButton) {
+        QPointF delta = event->position() - m_last_drag_pos;
+        m_last_drag_pos = event->position();
+        horizontalScrollBar()->setValue(horizontalScrollBar()->value() -
+                                        delta.x());
+        verticalScrollBar()->setValue(verticalScrollBar()->value() - delta.y());
+    }
 }
--- lib/imagepainter/imagepainter.hpp
+++ lib/imagepainter/imagepainter.hpp
@@ -1,31 +1,31 @@
 #pragma once
 
+#include <qevent.h>
 #include <qgraphicsscene.h>
+#include <qgraphicsview.h>
 #include <qobject.h>
 #include <qwidget.h>
 #include <QGraphicsPixmapItem>
 #include <QImage>
+#include <QScrollBar>
 
 #include "imageprocessor.hpp"
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class ImagePainter;
-}
-QT_END_NAMESPACE
-
-class ImagePainter : public QWidget {
+class ImagePainter : public QGraphicsView {
     Q_OBJECT
    public:
     explicit ImagePainter(ImageProcessor* processor, QWidget* parent = nullptr);
-
-    ~ImagePainter() override { delete m_ui; }
 
     void setView(const QImage& image);
 
    private:
     ImageProcessor* m_processor;
-    Ui::ImagePainter* m_ui;
-
+    QPointF m_last_drag_pos;
     QGraphicsScene m_scene;
+    std::unique_ptr<QGraphicsPixmapItem> m_pixmap_item;
+
+   protected:
+    void wheelEvent(QWheelEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
 };
